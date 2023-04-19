14e9d22a3fdf1193fee25011396c4be29a1157fc
Kiss Anikó <126045521+KissAniko@users.noreply.github.com>
2023-04-19 15:47:34 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Számlavezetés
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Tetel> tetelek = new List<Tetel>();   
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbLista.ItemsSource = tetelek;
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            String csvsor = $"{txtID.Text}; {dpDatum}; {txtOsszeg.Text}; {txtPartner.Text}; {txtfoKategoria.Text}; {txtalKategoria.Text}";
+            Tetel ujTetel = new Tetel (csvsor) ;
+            tetelek.Add(ujTetel) ;
+            lbLista.Items.Refresh();   // frissítés
+        }
+       
+        private void btnElment_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("tetelek.csv", append : true);
+            foreach ( var elem in tetelek)
+            {
+                String csvsor = $"{elem.Id}; {elem.Datum}; {elem.Osszeg}; {elem.PartnerNeve}; {elem.FoKategoria}; {elem.AlKategoria}"; 
+                // a TETEL.cs fájlban get--set.. ezek az 'elemek' ott olvashatók
+
+                sw.WriteLine();
+            }
+            sw.Close(); 
+        }
+    }
+}
---
+++ Tetel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Számlavezetés
+{
+    public class Tetel
+    {
+
+        int id;
+        DateTime datum;
+        int osszeg;
+        string partnerNeve;
+        string foKategoria;
+        string alKategoria;
+
+
+        public Tetel (string csvSor)
+        {
+            string[] mezok = csvSor.Split(',');
+
+            this.id = int.Parse(mezok[0]);
+            this.datum = Convert.ToDateTime(mezok[1]);
+            this.osszeg = int.Parse(mezok[2]);
+            this.partnerNeve= mezok[3];
+            this.foKategoria= mezok[4];
+            this.alKategoria = mezok[5];
+                                              
+        }
+        public int Id { get => id; set => id = value; }
+        // public DateTime Datum { get => datum; set => datum = value; }  // itt másodpercre pontosan írja ki az időt 12:00:00 órával kezdődően
+        // public DateTime Datum { get => datum; set => datum.Date.ToLongDateString(); } 
+        public string Datum { get => datum.Date.ToShortDateString(); }   // itt csak a nap, hónap és évet írja ki.
+        public int Osszeg { get => osszeg; }
+        public string PartnerNeve { get => partnerNeve; }
+        public string FoKategoria { get => foKategoria; }
+        public string AlKategoria { get => alKategoria; }
+    }
+
+}
